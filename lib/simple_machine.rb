e7686b6f8978bcac498a99d7c71b6546010fe5c9
Milan Burmaja <burmajam@gmail.com>
2010-12-13 01:34:44 +0100

    More instances in multi instance test

---
+++ lib/simple_machine.rb
@@ -0,0 +1,113 @@
+module SimpleMachine
+  def self.included(klass)
+    klass.extend ClassMethods
+  end
+  
+  class SimpleMachinePrototype
+    def initialize(owner)
+      @owner = owner
+    end
+    
+    def all_states
+      self.class.all_states
+    end
+    def allowed_transitions
+      current_state = @owner.send self.class.parents_state_field_name
+      result = self.class.defined_transitions[current_state].collect { |hash| hash[:transition] }
+      result.collect do |transition|
+        guard_method = "guard_for_#{transition}_on_#{self.class.parents_state_field_name}".to_sym
+        transition if !@owner.respond_to?( guard_method ) or @owner.send(guard_method)
+      end.compact
+    end
+    
+    class << self
+      attr_accessor :parents_state_field_name
+      attr_reader :all_states, :defined_transitions
+      attr_writer :owner_class
+      
+      def initial_state(state)
+        @all_states = [] << state
+        variable = "@#{@parents_state_field_name}_default_state"
+        @owner_class.instance_eval { instance_variable_set(variable, state) }
+      end
+      def other_states(*other_states)
+        @all_states = @all_states | other_states
+      end
+      def defined_transition?(transition, from_state)
+        return false unless @defined_transitions.has_key? from_state
+        result = @defined_transitions[from_state].inject(false) { |sum, hash| sum || hash[:transition] == transition }
+        return result
+      end
+      def allow_transition(transition, options)
+        @defined_transitions ||= {}
+        raise "Unknown source state #{options[:from]}. Please define it first as initial_state or in other_states." unless all_states.include?(options[:from])
+        raise "Unknown target state #{options[:to]}. Please define it first as initial_state or in other_states." unless all_states.include?(options[:to])
+        raise "Already defined transition '#{transition}' from '#{options[:from]}' state" if defined_transition?(transition, options[:from])
+        
+        @defined_transitions[options[:from]] = [] unless @defined_transitions.has_key?(options[:from])
+        @defined_transitions[options[:from]] << { :transition => transition, :target_state => options[:to] }     
+        
+        class_eval do
+          define_method "can_#{transition}?" do
+            allowed_transitions.include? transition
+          end
+          define_method transition do
+            current_state = @owner.send self.class.parents_state_field_name
+            if self.class.defined_transitions[current_state].inject(false) { |memo, hash| memo or hash[:transition] == transition }
+              raise "Unable to #{transition.to_s.gsub '_', ' '} due to guard"
+            else
+              raise "Invalid transition ##{transition.to_s.gsub '_', ' '} from '#{current_state}' state"
+            end unless allowed_transitions.include? transition
+            variable = "@#{self.class.parents_state_field_name}"
+            result = @owner.instance_eval { instance_variable_set variable, options[:to] }
+            callback_method = "after_#{self.class.parents_state_field_name}_changed".to_sym
+            @owner.send callback_method if @owner.respond_to? callback_method
+            result
+          end
+        end   
+      end
+    end
+  end
+
+  module ClassMethods
+      
+    def implement_state_machine_for(state_field_name, &block)
+      machine_property_name = "#{state_field_name}_machine".to_sym
+      default_state_field_name = "#{state_field_name}_default_state".to_sym
+
+      define_method state_field_name do
+        result = instance_variable_get "@#{state_field_name}"
+        result = instance_variable_set "@#{state_field_name}", self.class.send(default_state_field_name) unless result
+        result
+      end
+      define_method machine_property_name do
+        result = instance_variable_get "@#{machine_property_name}"
+        result = instance_variable_set "@#{machine_property_name}", SimpleMachine::ClassMethods.get_state_machine_class_for(self.class, state_field_name).new(self) unless result
+        result
+      end
+      self.class.class_eval do
+        define_method default_state_field_name do
+          instance_variable_get "@#{default_state_field_name}"
+        end
+      end
+      
+      SimpleMachine::ClassMethods.get_state_machine_class_for(self, state_field_name).instance_eval &block
+      
+      raise "Initial state not defined." unless send default_state_field_name
+    end
+    
+    private
+    
+    @state_machine_classes = {}
+    
+    def self.get_state_machine_class_for(cls, state_field_name)
+      key = [cls.to_s.to_sym, state_field_name]
+      unless @state_machine_classes.has_key? key
+        @state_machine_classes[key] ||= SimpleMachine::SimpleMachinePrototype.clone
+        @state_machine_classes[key].owner_class = cls
+        @state_machine_classes[key].parents_state_field_name = state_field_name
+      end
+      @state_machine_classes[key]
+    end
+  end
+end
---
+++ spec/spec_simple_machine.rb
@@ -0,0 +1,204 @@
+require 'rubygems'
+require 'rspec'
+require 'mocha'
+require File.expand_path(File.dirname(__FILE__) + '/../lib/simple_machine')
+
+RSpec.configure { |config| config.mock_with :mocha }
+
+describe SimpleMachine, "for :dispatch_state field on Job, when state machine is aready defined for :other_state on Job and for :dispatch_state on Driver" do
+  before :all do
+    class Driver
+      include SimpleMachine
+      implement_state_machine_for :dispatch_state do
+        initial_state :waiting
+        other_states :assigned, :circling, :going_to_pickup_location, :on_pickup_location, :driving_passenger, :changing_zone, :going_home
+        allow_transition :assign, :from => :waiting, :to => :assigned
+      end
+    end
+    class Job
+      include SimpleMachine  
+      implement_state_machine_for :other_state do
+        initial_state :created
+        other_states :deleted, :reviewed, :done, :closed
+        allow_transition :delete, :from => :created, :to => :deleted
+        allow_transition :delete, :from => :reviewed, :to => :deleted
+      end
+      # following commented out lines will be tested:
+      #
+      # implement_state_machine_for :dispatch_state do
+      #   initial_state :waiting
+      #   other_states :assigned, :cancelled
+      #   allow_transition :assign, :from => :waiting, :to => :assigned
+      #   allow_transition :cancel, :from => :waiting, :to => :cancelled
+      #   allow_transition :reject, :from => :assigned, :to => :waiting
+      # end
+      #
+      # def guard_for_cancel_on_dispatch_state; false; end
+    end
+
+    # Job.dispatch_state_default_state => :waiting
+    # job = Job.new
+    # job.dispatcn_state => :waiting
+    # job.dispatch_state_machine.allowed_transitions => [:assign]
+    # job.dispatch_state_machine.can_cancel? => false
+    # job.dispatch_state_machine.cancel => Exception: 'Unable to cancel due to guard'
+    # job.dispatch_state_machine.can_reject? => false
+    # job.dispatch_state_machine.reject => Exception: 'Invalid transition #reject from 'waiting' state'
+    # job.dispatch_state_machine.can_assign? => true
+    # job.dispatch_state_machine.assign => :assigned
+    # job.dispatcn_state => :assigned
+  end
+  context "without initial state" do
+    it "raises exception that initial state is undefined" do
+      lambda { Job.implement_state_machine_for :dispatch_state do; end }.should( raise_error do |e|
+        e.message.should match("Initial state not defined.")
+      end )
+    end
+  end
+
+  context "with only initial state :waiting" do
+    before :each do
+      Job.implement_state_machine_for :dispatch_state do
+        initial_state :waiting
+      end
+    end
+    it "defines :waiting as dispatch_state_default_state in Job" do
+      Job.dispatch_state_default_state.should be(:waiting)
+    end    
+    it "sets dispatch_status to :waiting on new job" do
+      Job.new.dispatch_state.should be(:waiting)
+    end
+
+    describe "#dispatch_state_machine on job " do
+      it "#all_states contains only :waiting" do
+        all_states = Job.new.dispatch_state_machine.all_states
+      
+        all_states.size.should == 1
+        all_states.should include(:waiting)
+      end    
+    end
+  end  
+
+  context "when initial :waiting and other :assigned, :accepted, :cancelled and :closed states are defined also" do
+    before :all do
+      Job.implement_state_machine_for :dispatch_state do
+        initial_state :waiting
+        other_states :assigned, :accepted, :cancelled, :closed
+      end
+    end
+    it "#dispatch_state_machine#all_states has 5 states" do
+      Job.new.dispatch_state_machine.all_states.should have(5).states
+    end
+
+    context "for defined :assign transition from :waiting to :assigned" do
+      before :all do
+        @inner_state_machine_class = Job.new.dispatch_state_machine.class
+        @inner_state_machine_class.allow_transition :assign, :from => :waiting, :to => :assigned
+      end
+      describe "#dispatch_state_machine#allowed_transitions" do
+        it "contains only :assign when in :waiting state" do
+          job = Job.new
+        
+          job.dispatch_state_machine.should have(1).allowed_transitions
+          job.dispatch_state_machine.allowed_transitions.should include(:assign)
+        end
+        it "doesn't contain :assign if job#guard_for_assign_on_dispatch_state returns false" do
+          Job.any_instance.stubs(:guard_for_assign_on_dispatch_state).returns false
+        
+          Job.new.dispatch_state_machine.should have(0).allowed_transitions
+        end
+        it "contains :assign if job#guard_for_assign_on_dispatch_state returns true" do
+          Job.any_instance.stubs(:guard_for_assign_on_dispatch_state).returns true
+        
+          Job.new.dispatch_state_machine.allowed_transitions.should include(:assign)
+        end
+        it "raises exception if there's attempt to define again :assign transition from :waiting state" do
+          @inner_state_machine_class = Job.new.dispatch_state_machine.class
+          lambda { @inner_state_machine_class.allow_transition :assign, :from => :waiting, :to => :closed }.should( raise_error do |e|
+            e.message.should match("Already defined transition 'assign' from 'waiting' state")
+          end )
+        end
+      end
+      describe "assign transition" do
+        context "when it is allowed" do
+          it "returns :assigned" do
+            job = Job.new
+            job.dispatch_state_machine.assign.should be(:assigned)
+          end
+          it "sets dispatch_state to :assigned" do
+            job = Job.new
+            job.dispatch_state_machine.assign
+          
+            job.dispatch_state.should be(:assigned)
+          end
+          it "calls job#after_dispatch_state_changed callback if defined" do
+            Job.any_instance.expects(:after_dispatch_state_changed)
+            Job.new.dispatch_state_machine.assign
+          end
+        end
+        context "when it is not valid transition from current state" do
+          it "raises an error" do
+            @inner_state_machine_class.allow_transition :reject, :from => :assigned, :to => :waiting
+          
+            job = Job.new
+            lambda { job.dispatch_state_machine.reject }.should( raise_error do |e|
+              e.message.should match("Invalid transition #reject from 'waiting' state")
+            end )
+          end
+        end
+        context "when it is not valid transition due to guard method" do
+          it "raises an error" do
+            Job.any_instance.stubs(:guard_for_assign_on_dispatch_state).returns false
+          
+            job = Job.new
+            lambda { job.dispatch_state_machine.assign }.should( raise_error do |e|
+              e.message.should match("Unable to assign due to guard")
+            end )
+          end
+        end
+      end
+      describe "transition guards" do
+        it "#can_assign? responds as true" do
+          job = Job.new
+          
+          job.dispatch_state_machine.allowed_transitions.should include(:assign)
+          job.dispatch_state_machine.can_assign?.should be_true
+        end
+        it "responds as false for #can_go_back? if :go_back is not allowed transition but is defined" do
+          @inner_state_machine_class.allow_transition :go_back, :from => :assigned, :to => :waiting
+          
+          job = Job.new
+          
+          job.dispatch_state_machine.allowed_transitions.should_not include(:go_back)
+          job.dispatch_state_machine.can_go_back?.should be_false
+        end
+        it "raises an error for #can_do_something_undefined? if :do_something_undefined is not defined transition" do
+          lambda { Job.new.dispatch_state_machine.can_do_something_undefined? }.should( raise_error do |e|
+            e.message.should include("undefined method `can_do_something_undefined?'")
+          end )
+        end
+      end
+    end
+  end
+  context "when there are more job instances" do
+    before :all do
+      Job.implement_state_machine_for :dispatch_state do
+        allow_transition :accept, :from => :assigned, :to => :accepted
+      end
+    end
+    
+    it "each instance tracks it's own flow" do
+      job1 = Job.new
+      job2 = Job.new
+      job3 = Job.new
+      
+      job1.dispatch_state_machine.assign
+      job2.dispatch_state_machine.assign
+      job2.dispatch_state_machine.accept
+      
+      job1.dispatch_state.should be(:assigned)
+      job2.dispatch_state.should be(:accepted)
+      job3.dispatch_state.should be(:waiting)
+    end
+  end
+end
